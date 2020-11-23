d7fc95b7b9bcba3b3a63c48c6f906d13d734e641
kathy-su <66896340+kathy-su@users.noreply.github.com>
2020-11-22 19:31:20 -0800

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -51,3 +51,75 @@ bool checkHorizontal(string board[4][4], string symbol) {
     }
     return false;
 }
+//check if the location is already occupied on the board
+bool checkLocation(string board[4][4], int& desired_location, int& row, int& col) {
+    //initializing variable
+    int location_on_board;
+    //checking location
+    //goes through every location on the board to see if the value equals to the user input
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            //converting integer to string
+            istringstream(board[i][j]) >> location_on_board;
+            //if value equals to the user input, the saves the row and the column location
+            if (location_on_board == desired_location) {
+                row = i;
+                col = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+//keeps track of the scores for Player X and Player O
+//sees which player won the game 
+bool checkGameWon(string board[4][4], string symbol, int& score_x, int& score_o) {
+    //checking the vertical way of winning
+    //adding score count to the winner (either Player X or Player O)
+    if (checkVertical(board, symbol)) {
+        if (symbol == "X") {
+            score_x++;
+        }
+        else {
+            score_o++;
+        }
+        printBoard(board);
+        cout << symbol << " is the Winner! Congrats!" << endl;
+        return true;
+    }
+    //checking the horizontal way of winning
+    //adding score count to the winner (either Player X or Player O)
+    else if (checkHorizontal(board, symbol)) {
+        if (symbol == "X") {
+            score_x++;
+        }
+        else {
+            score_o++;
+        }
+        printBoard(board);
+        cout << symbol << " is the Winner! Congrats!" << endl;
+        return true;
+    }
+    //checking the diagonal way of winning
+    //adding score count to the winner (either Player X or Player O)
+    else if (checkDiagonal(board, symbol)) {
+        if (symbol == "X") {
+            score_x++;
+        }
+        else {
+            score_o++;
+        }
+        cout << symbol << " is the Winner! Congrats!" << endl;
+        return true;
+    }
+    return false;
+}
+//checks when the game is tied and updates the number of ties
+bool checkGameTie(string board[4][4], string symbol, int counter, bool winner_determined, int& number_of_ties) {
+    if (counter == 15 && !winner_determined) {
+        number_of_ties++;
+        return true;
+    }
+    return false;
+}
